933e510f3f1a72c65399dcd337778690c98c3732
Matt Reynolds <mattreynolds84@outlook.com>
2017-07-12 13:03:00 -0400

    week 3

--- MyCoolApp/PersonService.cs
+++ MyCoolApp/PersonService.cs
@@ -18,9 +18,30 @@ namespace MyCoolApp
             {
                 person.IsMale = false;
             }
-            
 
             Console.WriteLine(person.DisplayName);
+        }
+
+        public List<Person> MakePeopleList(int listCount)
+        {
+            //do something logically here
+            return MakePeopleListPrivate(listCount);
+        }
+
+        private List<Person> MakePeopleListPrivate(int listCount)
+        {
+            var results = new List<Person>();
+
+            for (int i = 0; i < listCount; i++)
+            {
+                var person = new Person();
+                person.Age = 32;
+                person.FirstName = $"Matt{i}";
+                person.LastName = "Reynolds";
+                results.Add(person);
+            }
+
+            return results;
         }
     }
 }
--- MyCoolApp/Program.cs
+++ MyCoolApp/Program.cs
@@ -14,9 +14,20 @@ namespace MyCoolApp
             person.Age = 30;
             person.FirstName = "Mattias";
             person.LastName = "Reynaldus";
+            person.IsMale = true;
 
             var personService = new PersonService();
             personService.PrintName(person);
+
+            Console.ReadKey();
+            Console.WriteLine("Writing People List");
+
+            var people = personService.MakePeopleList(3);
+
+            foreach (var p in people)
+            {
+                personService.PrintName(p);
+            }
 
             Console.ReadKey();
         }
